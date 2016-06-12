dd1d7edb4e8321230eddf6d5ac53f8e58830e444
Dong Han <minicpp@gmail.com>
2016-06-12 06:53:50 -0400

    first commit

---
+++ Project01/app/src/main/java/com/course/project01/MainActivity.java
@@ -0,0 +1,51 @@
+package com.course.project01;
+
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.DialogInterface;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Button button;  //instance of the button object
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); //R is an automatically generated class for resources
+
+        button = (Button)findViewById(R.id.button); //find the button in the view layout "activity_mail.xml"
+        button.setOnClickListener(new ButtonClick());   //bind an event class to the button
+
+    }
+
+}
+
+class AlertDialogGetClick implements DialogInterface.OnClickListener{
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        dialog.cancel();    //hide the dialog
+    }
+}
+
+
+class ButtonClick implements View.OnClickListener{
+
+    private AlertDialog.Builder myDialog;   //the instance of an alert dialog
+    @Override
+    public void onClick(View v) {
+        myDialog = new AlertDialog.Builder(v.getContext()); //create an instance of an alert dialog
+        myDialog.setTitle("My first android App");  //set title of the alert dialog
+        myDialog.setMessage("Hello, this is Android tutorial.");    //Change it to your name
+        myDialog.setCancelable(true);   //what will happen if you change it to false
+        myDialog.setPositiveButton("OK", new AlertDialogGetClick());    //set a button in the alert dialog
+        myDialog.create();  //ask operating system to prepare the dialog
+        myDialog.show();    //show the dialog to the front of view
+    }
+}
